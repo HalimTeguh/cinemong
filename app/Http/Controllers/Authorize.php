fe94039fd6fd849310c53890bbe9f625a48ee197
Halim Teguh Saputro <2131710122@student.polinema.ac.id>
2023-07-20 21:42:07 +0700

    menyelesaikan fitur register

--- app/Http/Controllers/Authorize.php
+++ app/Http/Controllers/Authorize.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class Authorize extends Controller
 {
-    //
+    public function signin(){
+        return view('register', 
+        [
+         'title' => 'Sign in'   
+        ]);
+    }
+
+    public function signup(){
+        return view('login', 
+        [
+         'title' => 'Sign in'   
+        ]);
+    }
+
+    public function createUser(Request $request){
+        $validateData = $request->validate([
+            'username' => 'required|min:3|unique:users',
+            'name' => 'required',
+            'birth' => 'required|date',
+            'password' => 'required|min:8',
+            'confirm' => 'required|same:password',
+            'level' => ''
+        ]);
+
+        User::create($validateData);
+        return redirect('/login');
+    }
 }
